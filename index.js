851be4d1f23b4d3cd4eb6ec10348ad91f8f63e8d
ardyansuwono <153726290+ardyansuwono@users.noreply.github.com>
2025-11-02 01:46:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 // Middleware CORS (biar bisa diakses dari browser/hls.js)
 app.use((req, res, next) => {
